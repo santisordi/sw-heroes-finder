0216a1bd7c301e8c21a8cb36dc16914747262946
Santiago <santiagosordi@gmail.com>
2024-12-12 21:10:11 +0100

    creat flag for first input

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export function useSearch (){
     const [search, updateSearch] = useState('')
     const [error, setError] = useState<string | null>(null)
-    
+    const isFirstInput = useRef<boolean>(true) // flag para saber si el usuario uso o no el input
+
     useEffect(()=>{
+        
+    if(isFirstInput.current){
+            isFirstInput.current = search === ''
+            return
+          }
       if (search === ''){
         setError('Can search an empty Hero')
         return
